a5352926900605733a1372a365b113e023591136
Jorie Clark <jorie.clark@gmail.com>
2015-11-05 16:41:18 -0800

    fix user and snippet models & specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
-  validates :username, :password, length: { in: 6..10 }
+  validates :password, length: { in: 6..10 }
 
   has_many :snippets
   # has_many :favorites, dependent: :destroy
